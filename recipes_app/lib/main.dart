5f727d0e2d3c8277b709218d36c127e000157cb9
Filipe Pais <vitor.filipe.pais@gmail.com>
2020-04-28 11:23:37 +0100

    updated classes

--- recipes_app/lib/main.dart
+++ recipes_app/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'screens/categories_screen.dart';
-import 'screens/category_meals_screen.dart';
+import 'screens/category_meals_screen.dar';
 
 void main() => runApp(MyApp());
 
